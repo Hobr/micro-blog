98fe92ca0c30bf3566e08c4c73e69bffb52ea32e
Hobr <mail@hobr.site>
2026-04-06 21:52:08 +0800

    lint: 初始化

---
+++ eslint.config.ts
@@ -0,0 +1,73 @@
+import prettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import path from "node:path";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import { includeIgnoreFile } from "@eslint/compat";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+import svelteConfig from "./svelte.config.js";
+import ts from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+
+const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
+
+export default defineConfig([
+    includeIgnoreFile(gitignorePath),
+    js.configs.recommended,
+    ts.configs.recommended,
+    svelte.configs.recommended,
+    prettier,
+    tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        rules: {
+            "no-undef": "off",
+        },
+    },
+    {
+        files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                extraFileExtensions: [".svelte"],
+                parser: ts.parser,
+                svelteConfig,
+            },
+        },
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser },
+    },
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.jsonc"],
+        plugins: { json },
+        language: "json/jsonc",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.css"],
+        plugins: { css },
+        language: "css/css",
+        extends: ["css/recommended"],
+    },
+    eslintConfigPrettier,
+    eslintPluginPrettierRecommended,
+]);
